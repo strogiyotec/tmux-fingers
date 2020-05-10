0501b800db5aaee45af56eae47479ae5ef0dec1e
Jorge Morante <jorge@morante.eu>
2020-05-10 20:16:53 +0200

    change start->show-hints cmd

--- lib/fingers/commands/start.rb
+++ lib/fingers/commands/start.rb
@@ -4,7 +4,7 @@ class Fingers::Command::Start < Fingers::Command::Base
 
     cmd = "#{cli} show_hints #{input_method} #{original_pane_id} #{original_window_id}"
 
-    _window_id, pane_id = tmux.create_window("[fingers]", "bash --norc --noprofile -c '#{cmd}'", 80, 24)
+    _window_id, pane_id = tmux.create_window("[fingers]", cmd, 80, 24)
 
     original_pane = tmux.pane_by_id(original_pane_id)
 
